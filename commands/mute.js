90661673623d6a91064f18b3c1959e4e0dfb2047
awexxx <alex@plaguecraft.xyz>
2021-04-08 13:19:39 -0400

    added stuff
    appended the Mute command to send a notification n stuff

--- commands/mute.js
+++ commands/mute.js
@@ -10,7 +10,7 @@ module.exports = {
         
 
         if(!args.length) {
-                return message.reply(`You didn't specify a user!`);
+                return message.reply(`you didn't specify a user!`);
             }
 
         if (target) {
@@ -23,15 +23,23 @@ module.exports = {
                 memberTarget.roles.add(muteRole.id);
                 message.channel.send(`<@${memberTarget.user.id}> has been muted`);
                 console.log(`UID ${memberTarget} has been muted!`)
+                
+                    let messageArgs = args.join(' ');
                 const muteEmbed = new Discord.MessageEmbed()
             
             .setTitle('Muted!')
             .setThumbnail('https://plaguecraft.xyz/assets/img/logo.png')
             .setDescription(`${message.author} has muted ${memberTarget}!`)
             .setColor('#c7002e')
+            .setFooter('PCN Mutes')
+            .setTimestamp();
 
             const channel = client.channels.cache.find(channel => channel.name === "punishments")
             channel.send(muteEmbed)
+            const notichannel = client.channels.cache.find(channel => channel.name === "muted-notifications")
+                notichannel.send(`Hey ${memberTarget}, you were muted indefinitely.\nIf you'd like to appeal, please follow the steps listed in the **muted-users** channel.`)
+
+            
                 return
             }
             memberTarget.roles.add(muteRole.id);
@@ -48,6 +56,9 @@ module.exports = {
 
             const channel = client.channels.cache.find(channel => channel.name === "punishments")
             channel.send(muteTimedEmbed)
+
+            const notitimedchannel = client.channels.cache.find(channel => channel.name === "muted-notifications")
+            notitimedchannel.send(`Hey ${memberTarget}, you were muted for ${ms(ms(args[1]))}.\nIf you'd like to appeal, please follow the steps listed in the **muted-users** channel.`)
  
             setTimeout(function () {
                 memberTarget.roles.remove(muteRole.id);
