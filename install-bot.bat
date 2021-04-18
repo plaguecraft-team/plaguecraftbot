b534f32337d76c9a0ef85cd8ccf60d852ee700f9
awexxx <alex@plaguecraft.xyz>
2021-04-18 13:12:24 -0400

    Added install scripts

---
+++ install-bot.bat
@@ -0,0 +1,3 @@
+git clone https://github.com/plaguecraft-team/plaguecraftbot
+cd plaguecraftbot
+npm i
---
+++ install-bot.sh
@@ -0,0 +1,4 @@
+sudo git clone https://github.com/plaguecraft-team/plaguecraftbot
+cd plaguecraftbot
+sudo npm i
+node .
