0a967214b8575c694be444d53635a1d1740b0e6a
awexxx <alex@plaguecraft.xyz>
2021-04-19 10:21:57 -0400

    More Updates
    1 - Updated how commands are handled (subfolders)
    2 - Organized commands
    3 - Working on getting JSON to format properly, but getting Discord.js to translate that properly is horrifying lmao

--- handlers/command-handler.js
+++ handlers/command-handler.js
@@ -10,5 +10,35 @@ module.exports = (client, Discord) => {
         } else {
             continue;
         }
+        
+        const moderationCmds = fs.readdirSync('./commands/moderation').filter(file => file.endsWith('.js'));
+        for(const file of moderationCmds){
+            const command = require(`../commands/moderation/${file}`);
+            if(command.name){
+                client.commands.set(command.name, command);
+            } else {
+                continue;
+            }
+
+            const minecraftCmds = fs.readdirSync('./commands/moderation').filter(file => file.endsWith('.js'));
+            for(const file of minecraftCmds){
+                const command = require(`../commands/minecraft/${file}`);
+                if(command.name){
+                    client.commands.set(command.name, command);
+                } else {
+                    continue;
+                }
+
+                const generalCmds = fs.readdirSync('./commands/general').filter(file => file.endsWith('.js'));
+            for(const file of generalCmds){
+                const command = require(`../commands/general/${file}`);
+                if(command.name){
+                    client.commands.set(command.name, command);
+                } else {
+                    continue;
+                    }
+                }
+            }
+        }
     }
 }
